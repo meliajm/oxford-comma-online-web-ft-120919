aa5573fa4d4edc2de567b00eae7603eadbc6be86
melia miller <meliajm@gmail.com>
2019-12-10 04:26:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
  if array.length == 1  do 
    array[0]
- elsif array.length == 2 do
+ elsif array.length == 2
    "#{array[0]} and #{array[1]}"
- elsif array.length == 3 do 
+ elsif array.length == 3 
    "#{array[0]}, #{array[1]}, and #{array[2]}"
  else
   "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, and #{array[4]}}"
