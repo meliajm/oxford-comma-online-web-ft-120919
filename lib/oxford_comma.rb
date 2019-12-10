2169f39b0586f60212d9378b3046ea20a5d3b281
melia miller <meliajm@gmail.com>
2019-12-10 04:25:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
  if array.length == 1  do 
-   
+   array[0]
+ elsif array.length == 2 do
+   "#{array[0]} and #{array[1}"
+ elsif array.length == 3 do 
+   "#{array[0]}, #{array[1]}, and #{array[2]}"
+ else
+  "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, and #{array[4]}}"
  end
 end
