0b28c52897aa047d6eed33151c3963bae11aa712
melia miller <meliajm@gmail.com>
2019-12-10 04:54:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -19,18 +19,18 @@ require 'pry'
 def oxford_comma(array)
   # array.each_with_index do |fruit, index|
   if array.length == 1   
-  array[0]
-elsif array.length == 2
-  "#{array[0]} and #{array[1]}"
-elsif array.length == 3 
-  "#{array[0]}, #{array[1]}, and #{array[2]}"
-# else
-  array = array.insert(-2, "and")
+    array[0]
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  elsif array.length == 3 
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    array = array.insert(-2, "and")
   # a.insert(2, 99) 
-  comma_string = array.join(", ")
+    comma_string = array.join(", ")
   
-  comma_string.reverse.sub(",", " ").reverse
-  binding.pry 
+    comma_string.reverse.sub(",", " ").reverse
+  end
 end 
 
 
