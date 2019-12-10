612cc90ea913e4f5ec72885ba540d5bd032f2953
melia miller <meliajm@gmail.com>
2019-12-10 04:33:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,17 @@
-def oxford_comma(array)
- if array.length == 1   
-   array[0]
- elsif array.length == 2
-   "#{array[0]} and #{array[1]}"
- elsif array.length == 3 
-   "#{array[0]}, #{array[1]}, and #{array[2]}"
- else
-  "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]}, and #{array[5]}"
- end
-end
+# def oxford_comma(array)
+# if array.length == 1   
+#   array[0]
+# elsif array.length == 2
+#   "#{array[0]} and #{array[1]}"
+# elsif array.length == 3 
+#   "#{array[0]}, #{array[1]}, and #{array[2]}"
+# else
+#   "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]}, and #{array[5]}"
+# end
+# end
 
 # make a little method that does each something
 
 def oxford_comma(array)
-  
+  array.each_with_index do |fruit, index|
 end 
